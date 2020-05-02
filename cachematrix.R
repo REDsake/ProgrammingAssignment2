dc18ee11e672a645cee4e8f78a9902dd1f311914
Rajan Singh <singhrajan19148@gmail.com>
2020-05-02 06:12:23 +0530

    Wrote the solution for the programming assignment 3 in R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## saves the value of matrix x and its inverse in the global env
+## returns the list of the 4 complementary functions created by su
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xi <- NULL
+    set <- function(y){
+        x <<- y
+        xi <<- NULL
+    }
+    get <- function() x
+    setinv <- function(x_inv) xi <<- x_inv
+    getinv <- function() xi
+    list( set = set , get = get , setinv = setinv , getinv = getinv )
 }
 
 
-## Write a short comment describing this function
-
+## the function below will return the inverser of matrix x 
+## if x has been cached before then print GETTING CACHED DATA
+## else will compute the inverse 
+## and cache its value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    xi <- x$getinv()
+    if(!is.null(xi)){
+        print("getting cached data")
+        return(xi)
+    }
+    data <- x$get()
+    xi <- solve(data)
+    x$setinv(xi)
+    xi
 }
